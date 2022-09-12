62fbff408a8182d876bce4585ebe062c4f92fbf5
sebastian krnjus <sebastiankrnjus25@gmail.com>
2022-09-12 11:42:18 +0200

    Add project files.

---
+++ zadatakbr2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatakbr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string upis = Console.ReadLine();
+
+            Console.WriteLine(upis.Replace(' ', '_'));
+
+            Console.ReadKey();
+        }
+    }
+}
